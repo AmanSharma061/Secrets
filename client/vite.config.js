b7687f2d43e055d4911296b861e57e1783fb1295
Aman <b121061@iiit-bh.ac.in>
2024-01-17 14:36:26 +0530

    "added server url"

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://safekey-thth.onrender.com',
         changeOrigin: true
       }
     }
